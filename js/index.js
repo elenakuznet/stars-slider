b7bd86c3fc97eae4e8f7f31dc91f418ac848b0a2
Kuznetsova Elena <kuzn.elena@yahoo.com>
2024-02-02 13:34:47 +0500

    fix adaptive style

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     direction: 'horizontal',
     loop: true,
     effect: 'coverflow', // cards, coverflow, flip, fade, cube, slider - по умолчанию
@@ -8,13 +7,13 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 2, // кол-во активных слайдов
     centeredSlides: true, // центрирование слайдов
 
-    // If we need pagination
+    // Пагинация
     pagination: {
     el: '.swiper-pagination',
     clickable: true,
     },
 
-    // Navigation arrows
+    // Навигация
     navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -23,4 +22,21 @@ const swiper = new Swiper('.swiper', {
     // autoplay: {
     //     delay: 1000, //Задержка перед перелистыванием 1с = 1000мл/с
     // },
+
+    breakpoints: {
+            320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+            },
+            // when window width is >= 480px
+            480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+            },
+            // when window width is >= 640px
+            768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+            }
+        },
 });
